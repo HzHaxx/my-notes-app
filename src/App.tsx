791cfdfb563768d6fc410458be2638422e964ee2
HzHaxx <hamberzq@gmail.com>
2024-10-20 16:01:13 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Components
-// import NoteForm from './components/NoteForm';
-// import NotesList from './components/NotesList';
+import NoteForm from './components/NoteForm';
+import NotesList, { Note } from './components/NotesList';
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const addNote = (title: string, content: string) => {
+    const newNote = {
+      id: Date.now(),
+      title,
+      content
+    };
+  
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id: number) => {
+    setNotes(notes.filter(note => note.id !== id));
+  };
+
   return (
     <div className="App">
-      <h1>From Notes App</h1>
+      <h1>My Notes App</h1>
+      <NoteForm onSave={addNote} />
+      <NotesList notes={notes} onDeleteNote={deleteNote} />
     </div>
   );
 }
