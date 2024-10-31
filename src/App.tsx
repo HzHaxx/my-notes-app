ffccaed3823542d7b17a27361722081a6b330a86
HzHaxx <hamberzq@gmail.com>
2024-10-30 23:24:19 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 // Components
 import NoteForm from './components/NoteForm';
 import NotesList, { Note } from './components/NotesList';
+import { title } from 'process';
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
+  const [editingNote, setEditingNote] = useState<Note | null>(null)
 
   const addNote = (title: string, content: string) => {
     const newNote = {
@@ -23,8 +25,15 @@ function App() {
   return (
     <div className="App">
       <h1>My Notes App</h1>
-      <NoteForm onSave={addNote} />
-      <NotesList notes={notes} onDeleteNote={deleteNote} />
+      <NoteForm 
+        onSave={editingNote ? updateNote : addNote}
+        editingNote={editingNote}
+      />
+      <NotesList 
+        notes={notes} 
+        onDeleteNote={deleteNote}
+        onEditNote={startEditingNote}
+      />
     </div>
   );
 }
